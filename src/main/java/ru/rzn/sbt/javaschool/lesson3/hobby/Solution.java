0ab97aa3de01840ae5a2ecb83c2af6705b439fc5
PavelSenin <pavelsenin@mail.mail>
2018-12-30 18:02:50 +0300

    Добавил упражнение Хобби

---
+++ src/main/java/ru/rzn/sbt/javaschool/lesson3/hobby/Solution.java
@@ -0,0 +1,16 @@
+package ru.rzn.sbt.javaschool.lesson3.hobby;
+
+public class Solution {
+    /**
+     * Хобби
+     *
+     * Реализуйте метод detect таким образом, чтобы определить из входного массива students количество
+     * студентов, занимающихся музыкой {@link Rockstar}, студентов, увлекающихся мотоциклами {@link Biker} и студентов
+     * без хобби {@link Student} (такие студенты не должны быть ни {@link Rockstar}, ни {@link Biker}), и разместите
+     * результат в виде объекта класса {@link DetectionResult} с заполненными соответствующими полями.
+     * Случаи с null-ссылкой на массив и null-элементами массива следует обрабатывать.
+     */
+    public static DetectionResult detect(Object[] students) {
+        return null;
+    }
+}
